775db4d494d240455cf14334fd47de6dd9ad380b
Craig Domville <tathrim@gmail.com>
2015-02-10 11:37:06 +0000

    Use Appcompat Fragment class
    Reasoning for this was mentioned in the MoreFragment issue, so we can use Fragemtns on devices below Honeycomb.

--- app/src/main/java/uk/ac/lancs/aurorawatch/MoreFragment.java
+++ app/src/main/java/uk/ac/lancs/aurorawatch/MoreFragment.java
@@ -1,6 +1,6 @@
 package uk.ac.lancs.aurorawatch;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
